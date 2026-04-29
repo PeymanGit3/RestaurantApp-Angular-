19face44ef188e170e85c3ef83587652a6da37c3
Pheiman İsayev <peymanisayev13@gmail.com>
2026-04-29 15:21:54 +0400

    final adjustments1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, RouterLink,NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
